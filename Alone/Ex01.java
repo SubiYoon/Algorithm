c440c7c3ec0ad59e4cb4a65eb2665e86272e483b
YoonDongSub <yoondongsub92@gmail.com>
2022-11-04 14:08:01 +0900

    Ex01

--- Alone/Ex01.java
+++ Alone/Ex01.java
@@ -1,29 +1,52 @@
-package eLearning.algoritm.chapter05;
+package algoritmReport;
+
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Ex01 {
 
 	public static void main(String[] args) {
-		int n=10;
-		int[] arr= {7,5,9,0,3,1,6,2,4,8};
+		// [문제] 다중 for문을 이용하여 1~50사이의 값중에서 소수를 구하시오.
+		/* [출결과]
+			2 3 5 7 11 ... 47
+			개수 : 15
+		*/
 		
-		for(int i=0; i<n; i++) {
-			int min_index= i;
-			for(int j =i+1; j<n; j++) {
-				if(arr[min_index] > arr[j]) {
-					min_index=j;
+		//나의 해결법
+		TreeSet<Integer> set=new TreeSet<Integer>();
+		
+		for(int i=1; i<=50; i++) {
+			set.add(i);
+			for(int j=2; j<i; j++) {
+				if(i%j == 0) {
+					set.remove(i);
 				}
 			}
-			//스와이프
-			int temp=arr[i];
-			arr[i]= arr[min_index];
-			arr[min_index]=temp;
-			
 		}
 		
-		for(int i=0; i<n; i++) {
-			System.out.print(arr[i] + " ");
+		set.remove(1);
+		
+		Iterator<Integer> iter=set.iterator();
+		while(iter.hasNext()){
+			System.out.print(iter.next()+" ");
 		}
-
+		System.out.println("\n개수 : " + set.size());
+		
+		//다른 해결법
+		int count=0;
+		
+		for(int i=2; i<=50; i++) {
+			int j=0;
+			for(j=2; j<i; j++) {
+				if(i%j==0) {
+					break;
+				}
+			}
+			if(i==j) {
+				count++;
+				System.out.print(i+ " ");
+			}
+		}
+		System.out.print("\n개수 : " + count);
 	}
-
 }
