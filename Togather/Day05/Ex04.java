0ea07433eb104a7aadb13cbf2b88d4bc7616d7e8
YoonDongSub <yoondongsub92@gmail.com>
2022-12-03 19:33:31 +0900

    2022.12.03

---
+++ Togather/Day05/Ex04.java
@@ -0,0 +1,14 @@
+package Algorithm.Togather.Day05;
+//문자열 숫자로 변경
+
+public class Ex04 {
+	    public int solution(String s) {
+	        int answer = 0;
+
+	        if(1<=s.length() && s.length()<=5) {
+				answer = Integer.valueOf(s);
+	        }
+	        
+	        return answer;
+	    }
+	}
---
+++ Togather/Day21/Ex01.java
@@ -0,0 +1,30 @@
+package Algorithm.Togather.Day21;
+
+/*
+ 카톡아이디 추천
+ 길이 3~15
+ 아이디 구성 -> 알파벳 소문자, 숫자, - _ .(마침표 처음끝 안됨, 연속사용 안됨)
+ 공백에는 a를 대입
+ 길이2개 이하면 마지막문자 최소 3개까지 맞춤
+ 길이 16이상이면 16번째부터 다 버림
+ 
+ */
+
+public class Ex01 {
+	public String solution(String new_id) {
+        String answer = "";
+     
+        
+        return answer;
+        
+        
+        
+    }
+	public static void main(String[] args) {
+		Ex01 ex = new Ex01();
+		String str = "...!@BaT#*..y.abcdefghijklm";
+		ex.solution(str);
+		System.out.println((int)'.');
+	}
+
+}
