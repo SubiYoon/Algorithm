7c565a831115596bbd8dc7f847342aec31e5c76c
YoonDongSub <yoondongsub92@gmail.com>
2022-12-07 09:19:26 +0900

    2022.12.07

--- Programmers/Day22/Ex01.java
+++ Programmers/Day22/Ex01.java
@@ -1,5 +1,16 @@
 package Algorithm.Programmers.Day22;
 
 public class Ex01 {
-    
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex01 ex = new Ex01();
+        int[] arr1 = {1,2,3,1,2,3,1};
+        int[] arr2 = {4,1,2,2,4,4,4,4,1,2,4,2};
+        ex.solution(3, 4, arr1);
+        ex.solution(4,3,arr2);
+    }
 }
